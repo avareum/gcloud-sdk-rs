7d62b6dbd75faab77de20eb8e81f1e3df1cb7f1b
NiponChi <nipon.chi@gmail.com>
2022-09-11 23:00:25 +0700

    implemented create_id_token()

--- gcloud-sdk/src/token_source/auth_token_generator.rs
+++ gcloud-sdk/src/token_source/auth_token_generator.rs
@@ -10,6 +10,7 @@ pub struct GoogleAuthTokenGenerator {
     token_source_type: TokenSourceType,
     token_source: BoxSource,
     cached_token: Arc<RwLock<Option<Token>>>,
+    cached_id_token: Arc<RwLock<Option<Token>>>,
 }
 
 impl GoogleAuthTokenGenerator {
@@ -24,11 +25,14 @@ impl GoogleAuthTokenGenerator {
             token_source_type,
             token_source,
             cached_token: Arc::new(RwLock::new(None)),
+            cached_id_token: Arc::new(RwLock::new(None)),
         })
     }
 
     pub async fn clear_cache(&self) {
         let mut write_state = self.cached_token.write().await;
+        *write_state = None;
+        let mut write_state = self.cached_id_token.write().await;
         *write_state = None;
     }
 
@@ -52,6 +56,40 @@ impl GoogleAuthTokenGenerator {
                         }
                         _ => {
                             let new_token = self.token_source.token().await?;
+                            *write_token = Some(new_token.clone());
+                            debug!(
+                                "Created a new Google OAuth token. Expiring: {}. Source: {:?}",
+                                new_token.expiry, self.token_source_type
+                            );
+                            new_token
+                        }
+                    }
+                };
+                Ok(new_token)
+            }
+        }
+    }
+
+    pub async fn create_id_token(&self) -> crate::error::Result<Token> {
+        let existing_token: Option<Token> = {
+            let read_state = self.cached_id_token.read().await;
+            read_state.clone()
+        };
+
+        let now = Utc::now();
+
+        match existing_token {
+            Some(token) if token.expiry.gt(&now) => Ok(token),
+            _ => {
+                let new_token = {
+                    let mut write_token = self.cached_id_token.write().await;
+
+                    match write_token.as_ref() {
+                        Some(updated_token) if updated_token.expiry.gt(&now) => {
+                            updated_token.clone()
+                        }
+                        _ => {
+                            let new_token = self.token_source.identity().await?;
                             *write_token = Some(new_token.clone());
                             debug!(
                                 "Created a new Google OAuth token. Expiring: {}. Source: {:?}",
